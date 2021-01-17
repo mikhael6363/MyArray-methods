3645a42d0eb5cabfb58f023e1d81265c92e4abb7
Mikhael6363 <mikhael.6363@gmail.com>
2021-01-17 22:54:14 +0200

    Refactor MyArray methods to class

--- index.js
+++ index.js
@@ -1,149 +1,163 @@
 'use strict';
 
-function MyArray() {
-  this.length = 0;
-  for(let i = 0; i < arguments.length; i++) {
-    this.push(arguments[i]);
-  }
-}
-
-// 1. Method - MyArray.isMyArray(arg)
-MyArray.isMyArray = function isMyArray(obj) {
-  return obj instanceof MyArray;
-};
-
-function MyArrayProto() {
-
- // 2. Method .push()
- this.push = function push() {
-   for(let i = 0; i < arguments.length; i++) {
-     this[this.length++] = arguments[i];
-   }
-   return this.length;
- };
-
- // 3. Method .pop()
- this.pop = function pop() {
-   if(this.length === 0) {
-     return;
-   }
-   const lastItem = this[this.length - 1];
-   delete this[--this.length];
-   return lastItem;
- };
-
- // 4. Method .unshift()
- this.unshift = function unshift() {
-   for(let i = this.length - 1; i >= 0; i--) {                          
-     this[i + arguments.length] = this[i];
-     this[i] = arguments[i];
-   }
-   return this.length += arguments.length;
- };
-
- // 5. Method .shift()
- this.shift = function shift() {
-   if(this.length === 0) {
-     return;
-   }
-   const firstItem = this[0];
-   for(let i = 0; i < this.length; i++) {
-    this[i] = this[i + 1];
-  }
-  delete this[--this.length];  
-  return firstItem;
- };
-
- // 6. Method .concat()
-this.concat = function concat() {
-  const newArray = new MyArray();
-  for(let i = 0; i < this.length; i++) {
-    newArray.push(this[i]);
-  }
-  for(let i = 0; i < arguments.length; i++) {
-    for(let j = 0; j < arguments[i].length; j++) {
-      newArray.push(arguments[i][j]);
+class MyArray {
+  constructor() {
+    this.length = 0;
+    for(let i = 0; i < arguments.length; i++) {
+      this.push(arguments[i]);
     }
   }
-  // for(let i = 0; i < arguments.length; i++) {
-  //   newArray.push(arguments[i]);
-  // }
-  return newArray;
-}
-
- // 7. Method .reverse()
- this.reverse = function reverse() {
-  for(let i = 0; i < this.length / 2; i++) {
-    const savedItem = this[i]
-    this[i] = this[this.length - i - 1];
-    this[this.length - i - 1] = savedItem;
+  // 1. Method - MyArray.isMyArray(obj)
+  static isMyArray(obj) {
+    return obj instanceof MyArray; 
   }
-  return this;
-
-  // 2nd version 
-  //  const newRevesedArray = new MyArray();
-  //  for(let i = this.length - 1; i >= 0; i--) {
-  //    newRevesedArray.push(this[i]);
-  //  }
-  //  return newRevesedArray;
- };
- 
-
- // Advanced tasks
-
- // 8. Method .forEach()
- this.forEach = function forEach(func) {
-  for(let i = 0; i < this.length; i++) {
-    func(this[i], i, this);
-  }
-  return;
-};
-
- // 8. Method .map() // объединение forEach & filter
- this.map = function map(func) {
-  const newArray = new MyArray();
-   // 1st variant
-  // newArray.forEach().push(func(this[i]));
   
-  // 2nd variant
-   for(let i = 0; i < this.length; i++) {
-    newArray.push(func(this[i], i, this));
-  }
-  return newArray;
- };
-
-
- // Additional tasks
-
- // 9. Method .flat()
- this.flat = function flat() {
-   const newArray = new MyArray();
-
-   return newArray;
- };
-
- // 10. Method .every()
- this.every = function every(func) {
-  for(let i = 0; i < this.length; i++) {
-    if(!func([i], i, this)) {
-      return false;
+  // 2. Method .push()
+  push() {
+    for(let i = 0; i < arguments.length; i++) {
+      this[this.length++] = arguments[i];
     }
-    return true;
+    return this.length;
   }
-};
 
- // 11. Method .some()
- this.some = function some(func) {
-  for(let i = 0; i < this.length; i++) {
-    if(func(this[i], i, this)) {
+  // 3. Method .pop()
+  pop() {
+    if(this.length === 0) {
+      return;
+    }
+    const lastItem = this[this.length - 1];
+    delete this[--this.length];
+    return lastItem;
+  }
+
+  // 4. Method .unshift()
+  unshift() {
+    for(let i = this.length - 1; i >= 0; i--){
+      this[i + arguments.length] = this[i];
+      this[i] = arguments[i];
+    }
+    return this.length += arguments.length;
+  }
+
+  // 5. Method .shift()
+  shift() {
+    if(this.length === 0) {
+      return;
+    }
+    const firstItem = this[0];
+    for(let i = 0; i < this.length; i++) {
+      this[i] = this[i + 1];
+    }
+    delete this[--this.length];  
+    return firstItem;
+  }
+
+  // 6. Method .concat()
+  concat() {
+    const newArray = new MyArray();
+    for(let i = 0; i < this.length; i++) {
+      newArray.push(this[i]);
+    }
+    for(let i = 0; i < arguments.length; i++) {
+      for(let j = 0; j < arguments[i].length; j++) {
+        newArray.push(arguments[i][j]);
+      }
+    }
+    // for(let i = 0; i < arguments.length; i++) {
+    //   newArray.push(arguments[i]);
+    // }
+    return newArray;
+  }
+
+  // 7. Method .reverse()
+  reverse() {
+    for(let i = 0; i < this.length / 2; i++) {
+      const savedItem = this[i]
+      this[i] = this[this.length - i - 1];
+      this[this.length - i - 1] = savedItem;
+    }
+    return this;
+
+    // 2nd version 
+    //  const newRevesedArray = new MyArray();
+    //  for(let i = this.length - 1; i >= 0; i--) {
+    //    newRevesedArray.push(this[i]);
+    //  }
+    //  return newRevesedArray;
+  }
+
+
+  /* Advanced tasks */
+
+  // 8. Method .forEach()
+  forEach(func) {
+    for(let i = 0; i < this.length; i++) {
+      func(this[i], i, this);
+    }
+    return;
+  }
+
+  // 9. Method .map()
+  map(func) {
+    const newArray = new MyArray();
+    // 1st variant
+    // newArray.forEach().push(func(this[i]));
+  
+    // 2nd variant
+    for(let i = 0; i < this.length; i++) {
+      newArray.push(func(this[i], i, this));
+    }
+    return newArray;
+  }
+  
+
+  /* Additional tasks */
+
+  // 10. Method .flat()
+  flat(depth = 1) {
+    let newArray = new MyArray();
+    /* Императивный стиль кода: 
+    for (let i = 0; i < this.length; i++) {
+      if(MyArray.isMyArray(this[i]) && depth) { // или depth > 0
+        newArray = newArray.concat(this[i].flat(depth - 1));
+      } else if(this[i] !== undefined) {
+        newArray.push(this[i]);
+      }
+    } */
+
+    /* Функциональный (декларативный) стиль кода: */
+    this.forEach((item) => {
+      if(MyArray.isMyArray(item) && depth) { // или depth > 0
+        newArray = newArray.concat(item.flat(depth - 1));
+      } else if(item !== undefined) {
+        newArray.push(item);
+      }
+    });
+    return newArray;
+  }
+
+  // 11. Method .every()
+  every(func) {
+    for(let i = 0; i < this.length; i++) {
+      if(!func([i], i, this)) {
+        return false;
+      }
       return true;
     }
-    return false;
   }
-};
 
- // 12. Method .filter()
- this.filter = function filter(func) {
+  // 12. Method .some()
+  some(func) {
+    for(let i = 0; i < this.length; i++) {
+      if(func(this[i], i, this)) {
+        return true;
+      }
+      return false;
+    }
+  }
+
+  // 13. Method .filter()
+  filter(func) {
     const filteredNums = new MyArray();
     for(let i = 0; i < this.length; i++) {
       if(func(this[i], i, this)) {
@@ -151,16 +165,29 @@ this.concat = function concat() {
       }
     }
     return filteredNums;
-  };
+  }
+
+  // 14. Method [Symbol.iterator]()
+  [Symbol.iterator]() {
+    return new MyArrayIterator(this);
+  }
 }
 
-MyArray.prototype = new MyArrayProto();
+class MyArrayIterator {
+  /**
+   * 
+   * @param {MyArray} myArray 
+   */
+  constructor(myArray) {
+    this.array = myArray;
+    this.currentItem = 0;
+  }
+  next() {
+    return {
+      value: this.array[this.currentItem++],
+      done: this.currentItem > this.array.length,
+    }
+  }
+}
 
-const array1 = new MyArray(1, 2, 3, 4, 5);
-const array22 = new MyArray(1, 2, 3, 4, 5);
-const array2 = new Array(1, 2, 3, 4, 5);
-const array3 = [
-  {name: 'Mike', age: 18},
-  {name: 'Helen', age: 13},
-  {name: 'Brian', age: 23}
-];
+const array = new MyArray(1, 2, 3, 4, 5);
